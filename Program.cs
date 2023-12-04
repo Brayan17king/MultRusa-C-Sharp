9cf631be6ab5fa903551a3005fb58eacf8dcf1c2
Brayan17king <104658332+Brayan17king@users.noreply.github.com>
2023-12-04 17:36:12 -0500

    feat: :sparkles: Program Multiplicacion Rusa

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int multiplicador, multiplicando, a, b, c = 0;
+
+        Console.Write("Ingrese el multiplicador: ");
+        multiplicador = int.Parse(Console.ReadLine());
+        Console.Write("Ingrese el multiplicando: ");
+        multiplicando = int.Parse(Console.ReadLine());
+
+        a = multiplicador;
+        b = multiplicando;
+        while(a != 0)
+        {
+            if(a % 2 != 0)
+                c = c + b;
+
+            a = a / 2;
+            b = b * 2;    
+        }
+        Console.WriteLine($"Resultado: {c}");
+        Console.ReadKey();
+    }
+}
